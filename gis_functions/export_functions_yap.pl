0efc524f0107d770140db72226fa8e51b0e431a2
lourencoantunesPT <lourenco.antunes.pt@gmail.com>
2025-05-28 09:11:21 +0000

    fixes for YAP env

--- gis_functions/export_functions_yap.pl
+++ gis_functions/export_functions_yap.pl
@@ -4,6 +4,7 @@
 
 % Tell YAP about the shared object and which predicates it defines
 :- load_foreign_library('./yap2c_function.so').
+
 :- foreign_file('./yap2c_function', [
     transpose_geometry/4,
     disjoint_geometry/3,
